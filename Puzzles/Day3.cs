2a579e64942e757d8bda5e909c63a6c490b9f4f3
hashok <hars.ashok@gmail.com>
2022-10-14 16:39:43 -0700

    changes to bitArray name

--- Puzzles/Day3.cs
+++ Puzzles/Day3.cs
@@ -67,12 +67,13 @@ namespace AdventOfCode2021
         {
             ReadInputFile f = new ReadInputFile(sampleFile);
             BitArray[] bitsArray = f.ReadInputToByteArray();
+            BitArray[] oxygenRatings = new BitArray[bitsArray.Length];
+            bitsArray.CopyTo(oxygenRatings, 0);
 
             int j = 0;
-            while(bitsArray.Length > 1)
+            while(oxygenRatings.Length > 1)
             {
-                int listLength = bitsArray.Length;
-                int arraySize = bitsArray[0].Length;
+                int listLength = oxygenRatings.Length;
 
                 //counters to calculate column sums of 0's and 1's
                 int total_zeroes = 0;
@@ -84,39 +85,38 @@ namespace AdventOfCode2021
                 //Find the sum of each column
                 for (int i = 0; i < listLength; i++)
                 {
-                    if (bitsArray[i].Get(j))
+                    if (oxygenRatings[i].Get(j))
                     {
-                        ones_array.Add(bitsArray[i]);
+                        ones_array.Add(oxygenRatings[i]);
                         total_ones++;
                     }
                     else
                     {
-                        zero_array.Add(bitsArray[i]);
+                        zero_array.Add(oxygenRatings[i]);
                         total_zeroes++;
                     }
                 }
 
                 if (total_ones >= total_zeroes)
                 {
-                    bitsArray = ones_array.Cast<BitArray>().ToArray();
+                    oxygenRatings = ones_array.Cast<BitArray>().ToArray();
                 }
                 else
                 {
-                    bitsArray = zero_array.Cast<BitArray>().ToArray();
+                    oxygenRatings = zero_array.Cast<BitArray>().ToArray();
                 }
                 j++;
             }
 
             //copying the result because converter does something funky on mac intel processors.
-            int[] res = new int[bitsArray[0].Length];
-            for (int a = 0; a < bitsArray[0].Length; a++)
+            int[] res = new int[oxygenRatings[0].Length];
+            for (int a = 0; a < oxygenRatings[0].Length; a++)
             {
-                if (bitsArray[0].Get(a))
+                if (oxygenRatings[0].Get(a))
                     res[a] = 1;
                 else
                     res[a] = 0;
             }
-
             string result = string.Join("", res);
             Console.WriteLine("resultString : {0}", result);
             int o2 = Convert.ToInt32(result, 2);
